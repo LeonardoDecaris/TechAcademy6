18f2988f8e2737cb3f1cc4141e1b77ec3e3ca6b0
Leonardo Nunes Decaris <leonardodecaris8040@gmail.com>
2025-05-15 21:02:28 -0300

    testes de NAV

--- FrontEnd/tests/navBarTest.spec.ts
+++ FrontEnd/tests/navBarTest.spec.ts
@@ -48,4 +48,20 @@ test('Fazer logout', async ({ page }) => {
 });
 
 
+test('Botao GitHub', async ({ page }) => {
+    await page.goto('http://localhost:5173/home');
+    await page.click('text=GitHub');
+    const signUp = page.getByRole('button', { name: 'Sign up' });
+    await expect(page).toHaveURL('http://localhost:5173/login');
 
+    await page.locator('#email').fill('playwriteteste1@gmail.com');
+    await page.locator('#password').fill('MH4q8Da3JXp2Hye#$');
+    const loginButton = page.getByRole('button', { name: 'Login' });
+    await loginButton.scrollIntoViewIfNeeded();
+    await loginButton.click();
+
+    await expect(page).toHaveURL("http://localhost:5173/home");
+    const title = await page.getByText("The Power of audioVisual Harmonic Sound");
+    await page.waitForTimeout(500);
+    expect(title).toBeTruthy();
+});
