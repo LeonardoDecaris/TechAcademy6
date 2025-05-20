7fb24f7e0028d8b94addaa85eb39eb2fcb234f1b
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2025-05-20 18:51:27 -0300

    ajuste de  author

--- FrontEnd/src/components/custom/admin/BlocoAuthorCatg.tsx
+++ FrontEnd/src/components/custom/admin/BlocoAuthorCatg.tsx
@@ -1,18 +1,20 @@
+import AlertDeleteAuthor from "../error/AlertDeleteAuthor";
 import { ButtonClose } from "../global/GlobalButton";
 
 type Props = {
     Name: string;
-    onClick?: () => void;
+    idAuthor?: number;
 }
 
-const BlocoAuthorCatg = ({ Name, onClick }: Props) => {
+const BlocoAuthorCatg = ({ Name, idAuthor }: Props) => {
 
     const styleAuthor = "flex flex-col gap-1.5 bg-white/20 backdrop-blur-xl rounded-[10px] border-1 border-white font-bold text-white p-2.5 || max-w-[300px]";
-
     return (
         <div className={`${styleAuthor} `} data-aos="fade-up">
             <span className="font-medium" >Name: {Name}</span>
-            <ButtonClose children="Delete" onClick={onClick} buttonPosition="justify-center" />
+            <AlertDeleteAuthor IdAuthor={idAuthor!} >
+                <ButtonClose children="Delete" buttonPosition="justify-center" />
+            </AlertDeleteAuthor>
         </div>
     );
 };
