15051cdd5da3848b305d961fe1bbefa6f3ad9c4a
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2025-05-21 01:17:39 -0300

    category e author finalizados prontos para teste e ajuste finais

--- FrontEnd/src/hook/Categoary/hookDeleteCategory.ts
+++ FrontEnd/src/hook/Categoary/hookDeleteCategory.ts
@@ -4,8 +4,6 @@ function useHookDeleteCategory() {
   const handleDelete = async (deleteId: number) => {
     try {
       await api.delete(`/categories/${deleteId}`);
-
-      alert("Categoria deletada com sucesso!");
       location.reload();
     } catch (error) {
       alert("Erro ao deletar categoria. Tente novamente.");
