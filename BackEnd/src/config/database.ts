3308e0c65561aba7f4cdab1859a7018af367467c
Lucas Pedrozo <161781256+lucas-pedrozo@users.noreply.github.com>
2025-05-22 00:55:47 -0300

    Projeto com docker

--- BackEnd/src/config/database.ts
+++ BackEnd/src/config/database.ts
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize";
 const isTest = process.env.NODE_ENV === "test";
 
 const sequelize = new Sequelize(
-  isTest ? "harmonicsound_test" : "harmonicsound_homolog",
-  "root",
-  "",
+  (isTest ? process.env.DB_NAME_TEST : process.env.DB_NAME)!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
   {
-    host: "localhost",
+    host: process.env.DB_HOST!,
     dialect: "mysql",
     logging: !isTest,
   }
